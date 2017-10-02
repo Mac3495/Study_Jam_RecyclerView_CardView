4178ff85d3c0e96713f5ec1fb472ba47ad286e2f
Marcelo Torrez Azuga <Marcelo Torrez Azuga>
2017-10-01 21:14:33 -0400

    Primer commit

---
+++ app/src/main/java/com/example/marcelo/studyjamrecyclerview/DetalleActivity.java
@@ -0,0 +1,25 @@
+package com.example.marcelo.studyjamrecyclerview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class DetalleActivity extends AppCompatActivity {
+
+    ImageView imagenDetalle;
+    TextView nombre, descripcion;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle);
+
+        getSupportActionBar().setTitle("Descripción");
+
+        imagenDetalle = (ImageView) findViewById(R.id.imagen_detalle);
+        nombre = (TextView) findViewById(R.id.texto_detalle);
+        descripcion = (TextView) findViewById(R.id.descripcion_detalle);
+
+    }
+}
